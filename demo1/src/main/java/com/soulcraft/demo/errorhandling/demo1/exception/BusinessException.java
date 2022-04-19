1e38ba60ab7ff96a523857809ad4ce33d3c2528b
Scott Lau <exceedego@126.com>
2022-04-19 15:13:04 +0800

    first commit

---
+++ demo1/src/main/java/com/soulcraft/demo/errorhandling/demo1/exception/BusinessException.java
@@ -0,0 +1,30 @@
+package com.soulcraft.demo.errorhandling.demo1.exception;
+
+/**
+ * <p>
+ * business exception
+ * </p>
+ *
+ * @author Scott
+ * @since 20220411
+ */
+public class BusinessException extends RuntimeException {
+    public BusinessException() {
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ demo1/src/test/java/com/soulcraft/demo/errorhandling/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcraft.demo.errorhandling.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
