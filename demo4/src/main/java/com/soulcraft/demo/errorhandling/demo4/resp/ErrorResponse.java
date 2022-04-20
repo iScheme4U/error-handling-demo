eff89bf58ae5c41ea37883eb75eeb3950d32ff5b
Scott Lau <exceedego@126.com>
2022-04-20 09:54:26 +0800

    remove redundant methods

--- demo4/src/main/java/com/soulcraft/demo/errorhandling/demo4/resp/ErrorResponse.java
+++ demo4/src/main/java/com/soulcraft/demo/errorhandling/demo4/resp/ErrorResponse.java
@@ -9,21 +9,8 @@ package com.soulcraft.demo.errorhandling.demo4.resp;
  * @since 2022-03-10
  */
 public class ErrorResponse extends BaseResponse {
-    /**
-     * 构造错误返回信息
-     *
-     * @param code    返回码
-     * @param message 返回信息
-     */
-    public ErrorResponse(int code, String message) {
-        super(code, message);
-    }
 
     public ErrorResponse(IResponse response) {
-        this(response, response.getMessage());
-    }
-
-    public ErrorResponse(IResponse response, String message) {
-        this(response.getCode(), message);
+        super(response.getCode(), response.getMessage());
     }
 }
